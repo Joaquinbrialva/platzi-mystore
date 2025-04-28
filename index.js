f02aebacf3ad4af08255f30d4a79969fb32568a5
Joaquin Alvarez <joaquinbrialva@gmail.com>
2025-04-28 01:36:37 -0300

    Se implemento "morgan" y "cors"

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 const routerApi = require('./routes/index.routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 app.use(express.json());
+app.use(morgan('dev'));
+
+const whiteList = ['http://localhost:8080', 'http://127.0.0.1:8080'];
+const options = {
+  origin: (origin, cb) => {
+    if (whiteList.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error('no permitido'));
+    }
+  }
+};
+
+app.use(cors(options));
+
+routerApi(app);
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-routerApi(app);
 
 app.use(logErrors);
 app.use(boomErrorHandler);
