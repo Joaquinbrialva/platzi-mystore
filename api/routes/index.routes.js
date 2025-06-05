fd88b679e0dc45c5db10ee1d52beef6752c544ea
Joaquinbrialva <joaquinbrialva@gmail.com>
2025-06-04 22:34:05 -0300

    Añadir gestión de pedidos: crear rutas, servicios y esquemas para la nueva funcionalidad de pedidos

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -3,14 +3,16 @@ const productsRouter = require('./products.routes');
 const usersRouter = require('./users.routes');
 const customersRouter = require('./customers.routes');
 const categoriesRouter = require('./categories.routes');
+const ordersRouter = require('./orders.routes');
 
 function routerApi(app) {
-  const router = express.Router();
-  app.use('/api/v1', router);
-  router.use('/products', productsRouter);
-  router.use('/users', usersRouter);
-  router.use('/customers', customersRouter);
-  router.use('/categories', categoriesRouter);
+	const router = express.Router();
+	app.use('/api/v1', router);
+	router.use('/products', productsRouter);
+	router.use('/users', usersRouter);
+	router.use('/customers', customersRouter);
+	router.use('/categories', categoriesRouter);
+	router.use('/orders', ordersRouter);
 }
 
 module.exports = routerApi;
---
+++ api/services/order.service.js
@@ -0,0 +1,35 @@
+const { models } = require('../../libs/sequelize');
+
+class OrderService {
+	constructor() {}
+
+	async create(data) {
+		const newOrder = await models.Order.create(data);
+		return newOrder;
+	}
+
+	async find() {}
+
+	async findOne(id) {
+		const order = await models.Order.findByPk(id, {
+			include: [
+				{
+					association: 'customer',
+					include: ['user'],
+				},
+			],
+		});
+		return order;
+	}
+
+	async update(id, changes) {
+		return { id, changes };
+	}
+
+	async delete(id) {
+		const order = await this.findOne(id);
+		return { id };
+	}
+}
+
+module.exports = OrderService;
